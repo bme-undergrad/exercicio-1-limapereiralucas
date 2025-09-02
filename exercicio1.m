a6d916e8ab9f34493af9405dc6c7920febe3a049
limapereiralucas <limapereiralucas@gmail.com>
2025-09-02 16:23:05 -0300

    exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -6,10 +6,20 @@ imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% digite seu codigo aqui
-t = 0; % alterar
-
+t = zeros(imax, 1);
+    t(1) = x0;
+    
+    for ii = 1:length(t)-1
+        if ii ~= 1
+            erro(ii) = abs((t(ii)-t(ii-1))/t(ii)) * 100;  % erro relativo em percentual
+            if erro(ii) < es
+                break
+            endif
+        endif
+        t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii));
+    endfor
+    
+    t = t(ii);  % retorna a última estimativa válida
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
