b9f0e8a2cbe8e410d0e2379a8813ac5f8e0eae08
limapereiralucas <limapereiralucas@gmail.com>
2025-09-02 16:43:37 -0300

    exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,17 +1,16 @@
-function t = exercicio1(func,x0)
+function t = exercicio1(func, func_d, x0)
 
 % nao alterar: inicio
 es = 1;
 imax = 20;
 % nao alterar: fim
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
 t = zeros(imax, 1);
     t(1) = x0;
     
     for ii = 1:length(t)-1
         if ii ~= 1
-            erro(ii) = abs((t(ii)-t(ii-1))/t(ii)) * 100;  % erro relativo em percentual
+            erro(ii) = abs((t(ii)-t(ii-1))/t(ii)) * 100;
             if erro(ii) < es
                 break
             endif
